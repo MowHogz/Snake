443796851e5f1c1ff136cbe5ad9f486ba603f014
MowHogz <icreatemyweb@gmail.com>
2020-01-02 02:32:08 +0200

    Revert "v4.0 - Beta"

--- kbr.py
+++ kbr.py
@@ -1,8 +1,8 @@
 import keyboard
-
-def press(game):
-    k1 = ['w','s','d','a']
-    k2 = ['u','d','r','l']
-    for key in range(4):
-        if k2[key] not in game.snake.keys and keyboard.is_pressed(k1[key]):
-            game.snake.keys.append(k2[key])
+def press(d):
+    keys = ['w','s','d','a']
+    for key in keys:
+        if keyboard.is_pressed(key):
+            return key
+    return d
+    
